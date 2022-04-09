48ec5322db89ed05bcbdaa9ae51a6be58214c5d4
hannahvermaeten <hannahvermaeten@hotmail.com>
2022-04-09 12:36:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
 # 🚨 Do Not Change the code above 👆
 
-even_numbers = [n for n in numbers if (n%2)==0]
+with open("file1.txt") as file1:
+    file_1_data = file1.readlines()
+with open("file2.txt") as file2:
+    file_2_data = file2.readlines()
 
-print(even_numbers)
+results = [int(n) for n in file_1_data if n in file_2_data]
+print(results)
